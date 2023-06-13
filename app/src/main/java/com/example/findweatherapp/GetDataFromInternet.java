71072244a8754e896807a1b5c4be8333a4f804d5
Roman <3654004@gmail.com>
2023-06-13 08:07:50 +0500

    tty

--- app/src/main/java/com/example/findweatherapp/GetDataFromInternet.java
+++ app/src/main/java/com/example/findweatherapp/GetDataFromInternet.java
@@ -1,4 +1,25 @@
 package com.example.findweatherapp;
 
-public class GetDataFromInternet {
+import android.os.AsyncTask;
+import android.util.Log;
+
+public class GetDataFromInternet extends AsyncTask {
+    private static final String TAG = "GetDataFromInternet";
+    @Override
+    protected void onPreExecute() {
+        Log.d(TAG, "onPreExecute: called");
+        super.onPreExecute();
+    }
+
+    @Override
+    protected void onPostExecute(Object o) {
+        Log.d(TAG, "onPostExecute: called");
+        super.onPostExecute(o);
+    }
+
+    @Override
+    protected Object doInBackground(Object[] objects) {
+        Log.d(TAG, "doInBackground: called");
+        return null;
+    }
 }
